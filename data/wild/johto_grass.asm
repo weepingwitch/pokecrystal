18dbbc2856882ba845b1eeedf9efbd6a435a47a8
willow <willow@willow.lol>
2024-12-11 22:38:41 -0700

    route 29 changes

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1239,27 +1239,27 @@ JohtoGrassWildMons:
 	; morn
 	db 2, PIDGEY
 	db 2, SENTRET
-	db 3, PIDGEY
+	db 3, SPEAROW
 	db 3, SENTRET
 	db 2, RATTATA
 	db 3, HOPPIP
-	db 3, HOPPIP
+	db 2, BULBASAUR
 	; day
 	db 2, PIDGEY
 	db 2, SENTRET
-	db 3, PIDGEY
-	db 3, SENTRET
+	db 3, NIDORAN_M
+	db 3, NIDORAN_F
 	db 2, RATTATA
 	db 3, HOPPIP
-	db 3, HOPPIP
+	db 3, CHIKORITA
 	; nite
 	db 2, HOOTHOOT
-	db 2, RATTATA
+	db 2, EKANS
 	db 3, HOOTHOOT
 	db 3, RATTATA
 	db 2, RATTATA
 	db 3, HOOTHOOT
-	db 3, HOOTHOOT
+	db 3, ODDISH
 	end_grass_wildmons
 
 	def_grass_wildmons ROUTE_30
