f07856233ef77abad5614cdff6f3a7f067e7ec0f
willow <willow@willow.lol>
2024-12-17 15:15:55 -0700

    adding more balanced probabilities

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -5,13 +5,13 @@ ENDM
 
 GrassMonProbTable:
 	table_width 2, GrassMonProbTable
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 27,  0 ; 27% chance
+	mon_prob 50,  1 ; 23% chance
+	mon_prob 67,  2 ; 17% chance
+	mon_prob 80,  3 ; 13% chance
+	mon_prob 90,  4 ;  10% chance
+	mon_prob 97,  5 ;  7% chance
+	mon_prob 100, 6 ;  3% chance
 	assert_table_length NUM_GRASSMON
 
 WaterMonProbTable:
