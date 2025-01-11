5131fa43abbb415aa2f910b586586aebc57fbb60
willow <willow@willow.lol>
2025-01-10 23:16:07 -0700

    fixing new fairy moves

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -253,9 +253,9 @@ BattleAnimations::
 	dw BattleAnim_RockSmash
 	dw BattleAnim_Whirlpool
 	dw BattleAnim_BeatUp
-	dw BattleAnim_Dummy
-	dw BattleAnim_Dummy
-	dw BattleAnim_Dummy
+	dw BattleAnim_Flash
+	dw BattleAnim_Psybeam
+	dw BattleAnim_SweetKiss
 	assert_table_length NUM_ATTACKS + 1
 	dw BattleAnim_SweetScent2
 	assert_table_length $100
