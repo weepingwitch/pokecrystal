7a65f4041d963064d8fa3b75fd0ce73e19ac4502
willow <willow@willow.lol>
2025-01-18 13:35:14 -0700

    fightable at home elm?

--- maps/ElmsHouse.asm
+++ maps/ElmsHouse.asm
@@ -1,11 +1,39 @@
 	object_const_def
 	const ELMSHOUSE_ELMS_WIFE
 	const ELMSHOUSE_ELMS_SON
+	const ELMSHOUSE_ELM
 
 ElmsHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+ElmBattle:
+	readvar VAR_BADGES
+	if_greater_than 7, .doBattle
+	faceplayer
+	opentext
+	writetext ElmNotYetText
+	waitbutton
+	closetext
+	end
+.doBattle:
+	faceplayer
+	opentext
+	writetext ElmBattleText
+	waitbutton
+	closetext
+	winlosstext ElmBeatenText, 0
+	loadtrainer POKEMON_PROF, ELM
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext ElmBackToWorkText
+	waitbutton
+	closetext
+	playsound SFX_WARP_TO
+	applymovement ELMSHOUSE_ELM, ElmToWorkMovement
+	disappear ELMSHOUSE_ELM
 
 ElmsWife:
 	jumptextfaceplayer ElmsWifeText
@@ -18,6 +46,44 @@ ElmsHousePC:
 
 ElmsHouseBookshelf:
 	jumpstd DifficultBookshelfScript
+
+ElmToWorkMovement:
+	teleport_from
+	step_end
+
+ElmBackToWorkText:
+	text "Time to get"
+	line "back to the"
+	cont "lab!"
+	done
+
+ElmNotYetText:
+	text "Come back one"
+	line "evening, when"
+	cont "you have all"
+
+	para "8 JOHTO badges!"
+	done
+
+ElmBattleText:
+	text "Ok, <PLAY_G>,"
+	line "let's battle!"
+
+	para "Show me what"
+	line "you've learned!"
+	done
+
+ElmBeatenText:
+	text "Wow, good job!"
+
+	para "More people in"
+	line "NEW BARK TOWN"
+	cont "will battle you"
+
+	para "after you have"
+	line "16 badges!"
+
+	done
 
 ElmsWifeText:
 	text "Hi, <PLAY_G>! My"
@@ -84,3 +150,4 @@ ElmsHouse_MapEvents:
 	def_object_events
 	object_event  1,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, ElmsWife, -1
 	object_event  5,  4, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ElmsSon, -1
+	object_event  2,  1, SPRITE_ELM, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, NITE, 0, OBJECTTYPE_SCRIPT, 0, ElmBattle, -1
